212601b56058a8024d2d52635aceb4de84f9e82b
selvi8308 <142449128+selvi8308@users.noreply.github.com>
2023-08-17 20:00:37 -0500

    say hello world

--- hello.js
+++ hello.js
@@ -3,8 +3,7 @@ Welcome, new coder.
 Enter your code on line 7 to print 'Hello, World!' 
 to the console (the panel on the right)
 */
-
-console.log("Hello");
+console.log("hello all");
 
 /*  
 Next, PLAY! 
